788c8b8dcd24fda680c095cb99adb47f81568035
danilink <byron.lema@gmail.com>
2018-01-21 11:24:31 +0100

    make it better

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  db: process.env.MONGODB_URL || 'mongodb://localhost:27017/restaurant',
+  db: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/restaurant',
   auth: {
     secret: process.env.SECRET || 'apirestaurant',
   }
